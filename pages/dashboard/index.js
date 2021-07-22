74645095e883b5863ca6d580fdf96166ecc6ed74
Brian Ochan <bochan@outbox.co.ug>
2021-07-22 10:41:32 +0300

    Feature: Layout and Header components.
    - Added a Layout component to manage app layout.
    - Header component with icons

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,20 +1,9 @@
-import Head from 'next/head';
+import Layout from '../../components/Layout';
 
-export default function Dashboard() {
-  return (
-    <div>
-      <Head>
-        <title>Square Dashboard</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
-        <h1 className='text-6xl font-bold'>
-          Welcome to{' '}
-          <a className='text-blue-600' href='https://squareup.com/dashboard/'>
-            Square Dashboard
-          </a>
-        </h1>
-      </main>
-    </div>
-  );
+export default function Index() {
+  return <h2>Dashboard</h2>;
 }
+
+Index.getLayout = function getLayout(page) {
+  return <Layout title='Square Dashboard'>{page}</Layout>;
+};
